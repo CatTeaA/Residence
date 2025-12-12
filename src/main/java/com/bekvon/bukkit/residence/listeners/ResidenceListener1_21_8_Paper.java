6bc7df9b372925e66bbd5c8fdf57c5061ebaedac
Max <137968615+Huge-mistake@users.noreply.github.com>
2025-12-12 21:34:49 +0800

    Update ResidenceListener1_21_8_Paper.java

--- src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_21_8_Paper.java
+++ src/main/java/com/bekvon/bukkit/residence/listeners/ResidenceListener1_21_8_Paper.java
@@ -30,7 +30,7 @@ public class ResidenceListener1_21_8_Paper implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onKnockbackTest(EntityKnockbackEvent event) {
 
-        if (event.getCause() != io.papermc.paper.event.entity.EntityKnockbackEvent.Cause.UNKNOWN)
+        if (event.getCause() != io.papermc.paper.event.entity.EntityKnockbackEvent.Cause.ENTITY_ATTACK)
             return;
 
         Entity entity =  event.getEntity();
